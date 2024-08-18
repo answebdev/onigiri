e1d8f0c8bf5f0b493a23ccb49000cecfc325779e
unknown <britvaica@netscape.net>
2024-08-18 16:23:24 -0700

    initial commit

---
+++ story_content/triggers.js
@@ -0,0 +1,18 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6B03fs106Ta":
+        Script1();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
